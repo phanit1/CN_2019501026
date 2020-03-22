101c55c536aac3462afd0371311f247c2e19bad7
phanit1 <babithota5@msitprogram.net>
2020-03-22 21:11:42 +0530

    ProjectDone

--- Project/hangman_server.py
+++ Project/hangman_server.py
@@ -1,71 +1,146 @@
 import socket, random
 import sys
+import threading
 
-def isWordGuessed(secretWord, lettersGuessed):
-  for i  in secretWord:
-    if i not in lettersGuessed:
-      return False
-  return True
+def isWordGuessed(secretWord,letterGuessed) :
+    c = 0
+    for i in secretWord:
+        if i in letterGuessed:
+            c += 1
+    if c == len(secretWord):
+        return True
+    return False
 
-def getGuessedWord(secretWord, lettersGuessed):
-  r=""
-  for i in secretWord:
-    if i in lettersGuessed:
-      r = r + i
+def getGuessedWord(secretWord,lettersGuessed) :
+    l1 = []
+    for i in range(len(secretWord)):
+        l1.append("_")
+    for i in range(len(secretWord)):
+        if secretWord[i] in lettersGuessed:
+            l1[i] = secretWord[i]
+    return " ".join(l1)
+
+def getAvailableLetters(lettersGuessed) :
+    x = "abcdefghijklmnopqrstuvwxyz"
+    y = list(x)
+    for i in lettersGuessed:
+        y.remove(i)
+    return " ".join(y)
+
+def hangman(secretWord, player_name, connection) :
+    l = len(secretWord)
+    guesses = 8
+    wrongGuess_count = 0
+    lettersGuessed = []
+    welcomeMsg = "Welcome to the game, Hangman!" + str(player_name.user) + "\nI am thinking of a word that is " + str(l) + " letters long" + "\nYou have guesses " + str(guesses) + " left"
+    connection.send(welcomeMsg.encode())
+    
+    while guesses != 0 :
+        user_guess = connection.recv(1024).decode()
+        if user_guess in lettersGuessed :
+            avail_letters = getAvailableLetters(lettersGuessed)
+            alreadyGuessed = "Already guessed " + str(user_guess) + ".\nTry another letter" + ".\nYou have " + str(guesses) + " remaining." + "\nAvailable letters: " + avail_letters
+            connection.send(alreadyGuessed.encode())
+            continue
+
+        if user_guess in secretWord :
+            lettersGuessed.append(user_guess)
+            word = getGuessedWord(secretWord, lettersGuessed)
+            avail_letters = getAvailableLetters(lettersGuessed)
+            correctGuess = "Correct guess\n" + word + " is guessed till now. " + "\nYou have " + str(guesses) + " remaining. " + "\nAvailable letters: " + avail_letters
+            isFound = isWordGuessed(secretWord, lettersGuessed)
+            if isFound == True :
+                N = len(secretWord)
+                M = wrongGuess_count
+                score = (10 - M) * N
+                player_name.score += score
+
+                sorted_users = sorted(users)
+                lb = list()
+                for i in sorted_users :
+                    lb.append(i.leaderBoard())
+                ld = "\n".join(lb)
+                gc = "You guessed the word correctly: " + str(secretWord) + "\nYou have remaining guesses: " + str(guesses) + "\nYour score: " + str(score) + "\n" + ld
+                connection.send(gc.encode())
+                connection.close()
+                break
+            connection.send(correctGuess.encode())
+            continue
+
+        else :
+            wrongGuess_count += 1
+            lettersGuessed.append(user_guess)
+            word = getGuessedWord(secretWord, lettersGuessed)
+            avail_letters = getAvailableLetters(lettersGuessed)
+            guesses -= 1
+            wrongGuess = "Wrong guess. " + word + "\nYou have remaining guesses: " + str(guesses) + "\nAvailable letters: " + avail_letters
+            connection.send(wrongGuess.encode())
+            continue
+    
+    if guesses == 0 :
+        score = 0
+        player_name.score += score
+        lostGame = "You lost the game. Try again!" + "\nThe secret word is: " + str(secretWord) + "\nYour score: " + str(score) + "\n"
+        connection.send(lostGame.encode())
+        connection.close()
+
+class Hangman_Users :
+
+    words = list()
+    def __init__(self, user, score, word):
+        self.user = user
+        self.score = score
+        self.words.append(word)
+
+    def leaderBoard(self) :
+        return '{},{}'.format(str(self.user).ljust(15), str(self.score))
+
+def th(s) :
+    conn, address = s.accept()
+    user_existing = conn.send("New user? or Existing user?".encode())
+    user_choice = conn.recv(1024).decode()
+    if (user_choice == "new") :
+        conn.send("Enter your user name: ".encode())
+        user_name = conn.recv(1024).decode()
+        user_names.append(user_name)
+        f = open("words.txt").read().split()
+        secretWord = random.choice(f)
+        print(secretWord)
+        user = Hangman_Users(user_name, 0, secretWord)
+        users.append(user)
+        hangman(secretWord, user, conn)
     else:
-      r = r + "_ "
-  return r
+        conn.send("Enter your user name: ".encode())
+        user_name = conn.recv(1024).decode()
+        u_name = None
+        for un in users :
+            if (user_name in un.user) :
+                u_name = un
+                break
+        f = open("words.txt").read().split()
+        secretWord = random.choice(f)
+        print(secretWord)
+        user = Hangman_Users(user_name, 0, secretWord)
+        hangman(secretWord, user, conn)
 
-def getAvailableLetters(lettersGuessed):
+def main() :
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('localhost', 8089))
+    s.listen()
+    n = 1000
+    th_user = list()
+    for i in range(n) :
+        t1 = threading.Thread(target = th, args = (s,))
+        th_user.append(t1)
+        th_user[i].start()
 
-  new = ""
-  strr="abcdefghijklkmnopqrstuvwxyz"
-  for i in strr:
-    if i not in lettersGuessed:
-      new = new + i
-  return new
+    for i in range(n) :
+        th_user[i].join()
 
-def hangman(s):
-  print("Welcome to the game, Hangman!")
-  print(f"I am thinking of a word that is {len(secretWord)} letters long")
-  print("-"*10)
-  guesses = 6
-  lettersGuessed = []
-  neww=[]
-  while guesses > 0:
-    print(f"You have {guesses} guesses left")
-    print("Available letters:", getAvailableLetters(lettersGuessed))
-    lg=serversocket.recv(bytes(buf)).encode()
-    lg=lg.lower()
-    sc="~!@#$%^&*()_+=~{}[]:;\"\'<,>.?/|\'1234567890\\"
-    if lg not in sc:
-        if lg in secretWord and lg not in lettersGuessed:
-          neww.append(lg)
-          print("Good Guess:", getGuessedWord(secretWord, neww)) 
-          print("-"*10)
-        elif lg not in secretWord and lg not in lettersGuessed:
-          guesses -= 1
-          print("Oops! That letter is not in my word:", getGuessedWord(secretWord, lettersGuessed))
-          print("-"*10)
-        elif lg in lettersGuessed:  
-          print("Oops! You've already guessed that letter:", getGuessedWord(secretWord, lettersGuessed))
-          print("-"*10)
-        if isWordGuessed(secretWord, neww): 
-          print("Congratulations, you won!")
-          break
-        lettersGuessed.append(lg)
-    else:
-      print("Enter a valid input")
-    if guesses == 0:
-        print("Sorry, you ran out of guesses")
-        print("SecretWord is " + secretWord)
+    print("Done")
 
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-serversocket.bind(('localhost', 8089))
-serversocket.listen(8) # become a server socket, maximum 5 connections		
-while True:
-   connection, address = serversocket.accept()
-   buf = connection.recv(64)
-   f = open("words.txt").read().split()
-   secretWord=random.choice(f)
-   hangman(secretWord)
+users = list()
+user_names = list()
+if __name__ == "__main__" :
+    main()
